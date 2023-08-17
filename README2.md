6917fe91e089977475731431a09ce1fd0ddafdb6
sanskaarz <sanskar786g@gmail.com>
2023-08-17 14:48:19 +0530

    new file: ReadMe 2.md

---
+++ README2.md
@@ -0,0 +1,18 @@
+Undefined - Exist but no value
+Not- Defined - Does not Exist
+
+
+If there exist Brackets {} or [] or () then variale is refrence
+if not Bracket exist then variable is primitive
+
+
+function = code ko naam dena
+
+arguments = real values jo hum dete hai
+parameters = variables jinme values store hoti hai
+
+array = to store more then one value to a variable
+
+{} = object
+
+method = property of object whose value is a function
---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// function till50(){for(var i= 25; i<50; i++){
+//     console.log(i);
+// }}
+
+
+// till50();
+
+// var arr = [1,2,3,4,5,6,7];
+// arr.pop();// remove value
+// arr.push(9);//add value
+// arr.unshift(152,[158,765]);//add value in starting
+// arr.splice(5,2);// remove 2 values from 5th index
+// console.log(a);
+
+var laptop = {
+    brand: "Asus",
+    model: "X515",
+    price: "53k",
+    ram: "8GB",
+    Memory: "512GB",
+    Disktype: "SDD"
+}

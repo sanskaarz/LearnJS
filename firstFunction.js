c04418a8723d2a2a1d8877df6719b25d953c757f
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-09-07 11:08:43 +0530

    My First Function Code

---
+++ firstFunction.js
@@ -0,0 +1,10 @@
+function eligiblity(age){
+    if(age >=18){
+      console.log("you are eligible to vote");
+    }
+    else{
+      console.log("you are not eligible to vote");
+    }
+  }
+  
+  eligiblity(prompt("enter age"));

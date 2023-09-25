d4219d6f3a4a394dfbb4fa684b181442f0ca8a4f
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-09-25 18:19:01 +0530

    feat: Sum till n numbers
    This commit adds a new function called sumtilln which calculates the sum of all numbers from 0 to a given input number, inclusive. The function uses a for loop to iterate through the numbers and accumulates the sum.

--- Extras/sum till n.js
+++ Extras/sum till n.js
@@ -1,3 +1,16 @@
+function sumtilln(n){
+    let sum =0;
+    for(i=0;i<=n;i++){
+    sum+=i;
+    }
+    return sum;
+}
+
+
+
+//                   OR
+
+
 let n = prompt("enter a number")
 let sum =0;
 for(i=0;i<=n;i++){

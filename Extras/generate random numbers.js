7171a9fc7ecc7e10a2f7a11ca49cfc6cede71b85
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-09-25 14:16:50 +0530

    feat: Generate random numbers within range.

---
+++ Extras/generate random numbers.js
@@ -0,0 +1,14 @@
+console.log(Math.floor(Math.random() *x) +y)
+
+// x = how much numbers
+// y= from where + 1
+
+
+
+// From 21 to 25
+
+console.log(Math.floor(Math.random() *5) +21)
+
+
+// from 86 to 92
+console.log(Math.floor(Math.random() *7) +86)

9807f458ec4c8bc80b8fe465be45303f48392324
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-09-27 15:32:18 +0530

    Random num generator

--- Extras/random numbers generate.js
+++ Extras/random numbers generate.js
@@ -1,15 +1,11 @@
-console.log(Math.floor(Math.random() * x) + y)
-// [y, x+y)
-// x+y = max number 
-// y= from where
-
-
-
+function generateRandom(start,end){
+    let diff = end - start;
+    return Math.floor(Math.random() * diff) + start}
+// [start, end)
+// start-end = diff
 
 
 // From 21 to 25
-
 console.log(Math.floor(Math.random() *5) +21)
-
 // from 86 to 92
 console.log(Math.floor(Math.random() *7) +86)

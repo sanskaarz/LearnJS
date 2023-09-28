1d61b77f858ef3e8a37742473cfaf6b5e0874af2
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-09-28 11:06:36 +0530

    feat: Remove element from array code.

---
+++ Extras/Remove a elment from array.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 3, 4, 5, 6, 2, 3]
+let num = parseInt(prompt("Enter the number, u want to remove"))
+
+// Method 1
+let arrNew = []
+for(let x of arr){
+  if(num!==x){
+    arrNew.push(x)
+  }
+}
+console.log(arrNew)
+
+
+// Method 2
+for (i=0;i<arr.length;i++) {
+  if (arr[i] ==  num) {
+    arr.splice(i, 1)
+  }
+}
+console.log(arr)

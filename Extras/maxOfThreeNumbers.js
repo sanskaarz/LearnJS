8660821c02b9d996e14398f3d84696b35b576173
Sanskar Gupta <sanskar786g@gmail.com>
2024-12-01 23:17:42 +0530

    print largest of three numbers

---
+++ Extras/maxOfThreeNumbers.js
@@ -0,0 +1,25 @@
+let a = 3;
+let b = 5;
+let c = 11;
+if (a > b) {
+    if (a > c) {
+        console.log(a);
+    } else {
+        console.log(c);
+    }
+} else {
+    if (b > c) {
+        console.log(b);
+    }
+    else {
+        console.log(c);
+    }
+}
+// let arr1 = prompt("enter number 1");
+// let arr11 = arr1.split(",");
+// let arr2 = prompt("enter number 2");
+// let arr22 = arr2.split();
+// let arr3 = prompt("enter number 3");
+// let arr33 = arr3.split()
+// let total = arr11.concat(arr22 , arr33)
+// console.log(Math.max(...total));

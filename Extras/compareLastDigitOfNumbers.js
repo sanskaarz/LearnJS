d664eeba51dc16bc21d225bbc496b4d5badac57e
Sanskar Gupta <sanskar786g@gmail.com>
2024-12-08 22:51:48 +0530

    Add script to compare last digits of two numbers

---
+++ Extras/compareLastDigitOfNumbers.js
@@ -0,0 +1,8 @@
+let a = 3465;
+let b = 5654566;
+let c = 11888;
+if (a % 10 == b % 10) {
+    console.log("Both numbers have the same last digit");
+} else {
+    console.log("Both numbers have different last digits");
+}
